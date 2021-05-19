caa38b3af8644cfd504b8833bed2ffa80d51c02c
JumpingPxl <jumpingpxl@gmail.com>
2021-05-19 20:31:20 +0200

    Change version to 2

--- src/main/java/de/jumpingpxl/labymod/customhitboxes/CustomHitboxes.java
+++ src/main/java/de/jumpingpxl/labymod/customhitboxes/CustomHitboxes.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 public class CustomHitboxes extends LabyModAddon {
 
-	public static final String VERSION = "1";
+	public static final String VERSION = "2";
 
 	private static Settings settings;
 
